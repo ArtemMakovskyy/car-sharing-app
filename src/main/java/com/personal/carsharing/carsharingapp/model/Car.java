03698ccacd63ebc55ef96dfe813d68804b0b2375
ArtemMakovskyy <photomak79@gmail.com>
2023-11-08 15:37:38 +0200

    Add car table into DB Create entity Car

--- src/main/java/com/personal/carsharing/carsharingapp/model/Car.java
+++ src/main/java/com/personal/carsharing/carsharingapp/model/Car.java
@@ -1,11 +1,51 @@
 package com.personal.carsharing.carsharingapp.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "cars")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE cars SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
     private String model;
+    @Column(nullable = false)
     private String brand;
-    private CarType type;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private Type type;
+    @Column(nullable = false)
+    @ColumnDefault("0")
     private Integer inventory;
+    @Column(name = "daily_fee", nullable = false)
+    @ColumnDefault("0.00")
     private BigDecimal dailyFee;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+    public enum Type {
+        SEDAN,
+        SUV,
+        HATCHBACK,
+        UNIVERSAL
+    }
 }
